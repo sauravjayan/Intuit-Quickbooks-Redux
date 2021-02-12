1cdee0dba2c528b71d212ebbf57980c4cd9cc2eb
vnijs <vnijs@ucsd.edu>
2021-02-12 09:24:03 +0000

    Upload repo using gitgadget

---
+++ utils/functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pyrsm as rsm
+
+
+def example():
+    text = """
+You just accessed a function from your first python packages!
+Change the code in utils/function.py to whatever you need for this assignment
+Use 'from utils import functions' to get access to your code
+You can add modules to import from by adding additional .py files to the 'utils' directory
+Note: If you make changes to the content of this file you will have to restart the notebook kernel to get the updates
+"""
+    print(text)
